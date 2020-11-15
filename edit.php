71730f6851e8870c409a20374bc63c5b1eedf659
phufarpsw <73683175+phufarpsw@users.noreply.github.com>
2020-11-15 22:49:13 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -22,9 +22,9 @@
         <label for="Username">Username :</label>
           <input type="text" class="form-control" name = "Name" id="idName" value="<?php echo "$row[Name]"; ?>" <br>
           <label for="Username">Comment :</label>
-          <input type="text" name = "Comment" id="idComment" value="<?php echo "$row[Comment]"; ?>" <br>
+          <input type="text" class="form-control" name = "Comment" id="idComment" value="<?php echo "$row[Comment]"; ?>" <br>
         <label for="Username">Link :</label>
-          <input type="text" name = "Link" id="idLink" value="<?php echo "$row[Link]"; ?>"> <br><br>
+          <input type="text" class="form-control" name = "Link" id="idLink" value="<?php echo "$row[Link]"; ?>"> <br><br>
           <input type="submit" id="commentBtn"class="btn btn-outline-warning">
     </div>
       </form>
